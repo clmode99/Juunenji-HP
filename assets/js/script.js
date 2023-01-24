72ef4c85c67cb6bf2c76d8d1d8f38eac06cbe327
Miyu Hara <clmode99@gmail.com>
2023-01-24 15:06:11 +0900

    [update]背景画像のレスポンシブ対応。動画は再生できていない(サーバーたてないと無理？)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,48 @@
+$(window).on('load', function(){
+	$(".youtube-area").addClass('appear');
+	$(".loading").addClass('disappear');
+});
+
+// YouTube API
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let ytPlayer;
+function onYouTubeIframeAPIReady() {
+	ytPlayer = new YT.Player('youtube', {
+		videoId: 'BHyJIiuSros',		// YouTubeのID
+		playerVars: {
+			playsinline: 1,
+			autoplay: 1,
+			fs: 0,
+			rel: 1,
+			controls: 0,
+			modestbranding: 1,
+			iv_load_policy: 3,
+			start: 0,
+		},
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+function onPlayerReady(event) {
+	event.target.mute();
+	event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+	if(event.data == YT.PlayerState.ENDED) {
+		event.target.playVideo();
+	}
+}
+
+// jQuery
 $(function () {
 
 });
