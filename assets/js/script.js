7188fdf835bfd621995cd17fff86d5a5a5fb8c48
Miyu Hara <clmode99@gmail.com>
2023-01-27 17:18:53 +0900

    [add]スライドショー実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,5 +44,11 @@ function onPlayerStateChange(event) {
 
 // jQuery
 $(function () {
-
+	$('.main-slider-list').slick({
+		arrows: false,
+		dots: false,
+		autoplay: true,
+		autoplaySpeed: 3000,		// 画像の保持時間(ms)
+		speed: 1000,					// 画像の切り替わる時間(ms)
+	});
 });
