5ea75d7305d3f295fd26952ccd25dea201a97f16
Miyu Hara <clmode99@gmail.com>
2023-02-08 17:43:12 +0900

    [add]タブレット版レスポンシブ対応開始、aboutまで作成完了

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 // jQuery
 $(function () {
-	$('.main-visual-inner').YTPlayer();
+	// 動画再生
+	// デバイス判定参考サイト：https://indoor-today.com/2230
+	var ua = navigator.userAgent;
+	if(!(ua.indexOf('iPhone') > -1 || 
+		(ua.indexOf('Android') > -1 && ua.indexOf('Mobile') > -1) ||
+		ua.indexOf('iPad') > -1 || ua.indexOf('Android') > -1)) {
+			$('.main-visual-inner').YTPlayer();
+		}
 
+	// スライドショー
 	$('.main-slider-list').slick({
 		arrows: false,
 		dots: false,
@@ -9,4 +17,28 @@ $(function () {
 		autoplaySpeed: 3000,		// 画像の保持時間(ms)
 		speed: 1000,				// 画像の切り替わる時間(ms)
 	});
+
+	// ハンバーガーメニュー
+	$('.button-header-hamburger').click(function() {
+		$(this).blur();
+
+		// すでにメニュ＝表示してるなら、さようなら(重複させないようにする)
+		if($('.header-inner-right-mb-menu-list-overlay')[0])
+			return false;
+
+		$('body').append('<div class="header-inner-right-mb-menu-list-overlay"></div>');
+		$('.header-inner-right-mb-menu-list-overlay').fadeIn();
+		$('.header-inner-right-mb-menu-list-outer').fadeIn();
+	});
+
+	// ハンバーガーメニューからの閉じるとき
+	$(".header-inner-right-mb-menu-list-close-button").unbind().click(function() {
+		$('.header-inner-right-mb-menu-list-outer').fadeOut();
+		$('.header-inner-right-mb-menu-list-overlay').fadeOut(function() {
+			$('.header-inner-right-mb-menu-list-overlay').remove();
+		});
+	});
+
+	
 });
+
