3937a502e2c55ed9d7470ca84db6908779b94303
Miyu Hara <clmode99@gmail.com>
2023-02-22 22:36:03 +0900

    [update]「年間行事」ページレスポンシブ完了(タブレット)

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,13 @@ $(function () {
 		centerMode: false,
 		focusOnSelect: true,
 	});
+	$(".main-event-slider-tablet").slick({
+		dots: true,
+		arrows: true,
+		autoplay: true,
+		autoplaySpeed: 3000,		// 画像の保持時間(ms)
+		speed: 1000,				// 画像の切り替わる時間(ms)
+	});
 
 	// ハンバーガーメニュー
 	$('.button-header-hamburger').click(function() {
